d957c2182098ca0280206468b63608f44d73a00b
Rita Dmitrenok <rita.ivanova1876@gmail.com>
2024-11-04 00:14:01 +0300

    add first screen and styles

---
+++ Food-Library/Food-Library/MyButtonStyle.swift
@@ -0,0 +1,18 @@
+//
+//  MyButtonStyle.swift
+//  Food-Library
+//
+//  Created by apple on 4.11.24.
+//
+
+import SwiftUI
+
+struct MyButtonStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MyButtonStyle()
+}
---
+++ Food-Library/Food-Library/TextStyle.swift
@@ -0,0 +1,18 @@
+//
+//  TextStyle.swift
+//  Food-Library
+//
+//  Created by apple on 4.11.24.
+//
+
+import SwiftUI
+
+struct TextStyle: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TextStyle()
+}
