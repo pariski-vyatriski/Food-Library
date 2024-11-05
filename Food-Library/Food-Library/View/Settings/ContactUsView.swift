e2721b0e26d0a135f10a6bbbafc2c870ef82b66e
Rita Dmitrenok <rita.ivanova1876@gmail.com>
2024-11-05 19:37:44 +0300

    created a screen with settings and screens coming from it

---
+++ Food-Library/Food-Library/View/Settings/ContactUsView.swift
@@ -0,0 +1,18 @@
+//
+//  bvjhfb.swift
+//  Food-Library
+//
+//  Created by apple on 5.11.24.
+//
+
+import SwiftUI
+
+struct ContactUsView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    ContactUsView()
+}
---
+++ Food-Library/Food-Library/View/Settings/TermOfUse.swift
@@ -0,0 +1,18 @@
+//
+//  TermOfUse.swift
+//  Food-Library
+//
+//  Created by apple on 5.11.24.
+//
+
+import SwiftUI
+
+struct TermOfUse: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermOfUse()
+}
