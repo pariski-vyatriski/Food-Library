47af628bd9abdbbb8166e2538bf6783ea5622824
Rita Dmitrenok <rita.ivanova1876@gmail.com>
2024-11-05 20:42:51 +0300

    update pickerview on CalculatorCalories screen

---
+++ Food-Library/Food-Library/View/enum.swift
@@ -0,0 +1,18 @@
+//
+//  enum.swift
+//  Food-Library
+//
+//  Created by apple on 5.11.24.
+//
+
+import SwiftUI
+
+struct enum: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    enum()
+}
